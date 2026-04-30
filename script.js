39fe7c3f02ac464518476d1547f7f31b7913d20d
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-04-30 20:36:28 +0300

    .

--- script.js
+++ script.js
@@ -137,10 +137,8 @@
 // const arr = [4, 5, 6];
 // const [numOne, numTwo, numThree] = arr;
 
-
-
-// Створіть клас Calculator, який буде мати наступні методи: add(), subtract(), multiply(), divide(). 
-// Кожен метод приймає два аргументи - числа, які необхідно обробити. Результатом кожного методу має бути результат відповідної 
+// Створіть клас Calculator, який буде мати наступні методи: add(), subtract(), multiply(), divide().
+// Кожен метод приймає два аргументи - числа, які необхідно обробити. Результатом кожного методу має бути результат відповідної
 // математичної операції.
 // const calculator = new Calculator();
 // console.log(calculator.add(2, 3)); // 5
@@ -167,7 +165,6 @@
 // console.log(calculator.divide(10, 2)); // 5
 // console.log(calculator.divide(10, 0)); // На нуль ділити не можна
 
-
 // Створіть клас Animal, який має властивість name. Створіть клас Dog, який наслідується від Animal і має властивість breed.
 //  Створіть об'єкт myDog класу Dog з ім'ям Buddy і породою Labrador Retriever.
 // const myDog = new Dog('Buddy', 'Labrador Retriever');
@@ -188,7 +185,6 @@
 
 // const myDog = new Dog('Jessy', 'Labrador Retriever');
 // console.log(myDog); // { name: 'Buddy', breed: 'Labrador Retriever' }
-
 
 // class Shape{
 //     constructor(color){
@@ -212,7 +208,6 @@
 // myCircle.draw(); // Drawing a circle with radius 5
 // console.log(myCircle.color); // "red"
 
-
 // class Person{
 //     constructor(name, age){
 //         this.name = name;
@@ -234,12 +229,8 @@
 // const john = new Employee("John", 32, "programmer");
 // console.log(john.getInfo()); // "John is a programmer and is 32 years old"
 
-
-
-
-// // Створіть форму з двома текстовими полями та кнопкою "Додати", 
+// // Створіть форму з двома текстовими полями та кнопкою "Додати",
 // // яка додає новий елемент li в ul, що містить текст, який введено в першому полі.
-
 
 // const inp = document.getElementById('inp-one');
 // const btn = document.getElementById('btn');
@@ -253,7 +244,7 @@
 // btn.addEventListener('click', () => {
 //     const li = document.createElement('li');
 //     list.appendChild(li);
-    
+
 //     li.innerHTML = `
 //     <p> ${inp.value} </p>
 //     <p> ${inp.value} </p>
@@ -261,12 +252,9 @@
 //     inp.value = '';
 // })
 
-
-
-
 // // В HTML є пустий список ul#ingredients.
 
-// // 
+// //
 // // В JS є масив рядків.
 
 // const ingredients = [
@@ -290,12 +278,8 @@
 // list.appendChild(fragment);
 
 // // Напиши скрипт, який для кожного елемента масиву
-// //  ingredients створить окремий li, після чого вставить всі li за одну 
+// //  ingredients створить окремий li, після чого вставить всі li за одну
 // //  операцію в список ul.ingredients. Для створення DOM-вузлів використовуй document.createElement().
-
-
-
-
 
 // Лічильник складається зі спана і кнопок, які повинні збільшувати і зменшувати значення лічильника на 1.
 
@@ -324,11 +308,8 @@
 //     span.textContent = count;
 // });
 
-
-
-
-// Змінити фон елементу з id="box" на жовтий, 
-// якщо користувач натискає клавішу y, та на зелений, якщо 
+// Змінити фон елементу з id="box" на жовтий,
+// якщо користувач натискає клавішу y, та на зелений, якщо
 // користувач натискає клавішу g, під час події keydown.
 
 // const box = document.getElementById('box');
@@ -340,8 +321,6 @@
 //         box.style.backgroundColor = 'green';
 //     }
 // })
-
-
 
 // Перемістити елемент з id="box" наліво, якщо користувач натискає стрілку вліво,
 //  та направо, якщо користувач натискає стрілку вправо, під час події keydown.
@@ -355,7 +334,6 @@
 //   top: 50%;
 //   transform: translate(-50%, -50%);
 // }
-
 
 // let position = 50;
 
@@ -371,12 +349,9 @@
 //     box.style.left = position + '%'
 // })
 
-
-
 // document.addEventListener('keydown', (e) => {
 //     console.log(e.code)
 // })
-
 
 // const input = document.getElementById('input');
 // const message = document.getElementById('message');
@@ -385,10 +360,9 @@
 //     message.textContent = input.value
 // })
 
-
 // Написати програму яка буде за допомогою миші рухати блок на екрані.
-//  Цей код повинен прослуховати подію mousemove на документі та рухати блок на відстань, 
-//  яку миша змістилася відносно центру блоку. Якщо вам потрібно рухати саме центр блоку, 
+//  Цей код повинен прослуховати подію mousemove на документі та рухати блок на відстань,
+//  яку миша змістилася відносно центру блоку. Якщо вам потрібно рухати саме центр блоку,
 //  ви можете додати до відстані половину ширини та висоти блоку. Можна використати код та підказки нижче 👇
 
 // const block = document.querySelector('.block');
@@ -397,7 +371,7 @@
 // document.addEventListener('mousemove', e => {
 //    // Отримуємо координати миші на екрані
 //   const y = e.clientY;
-//   const x = e.clientX; 
+//   const x = e.clientX;
 //   // Отримуємо координати центра блоку
 //   const rect = block.getBoundingClientRect();
 //   const centrX = rect.left + rect.width/2;
@@ -409,9 +383,6 @@
 //   block.style.left = block.offsetLeft + deltaX + 'px';
 //   block.style.top = block.offsetTop + deltaY + 'px';
 //  });
-
-
-
 
 // Створи веб-сторінку, де:
 // Користувач клікає в будь-яке місце сторінки.
@@ -433,13 +404,11 @@
 //   document.body.appendChild(dot);
 // })
 
-
-// Написати програму яка буде робити підсвічування активного елементу навігації 
-// при кліці. Коли користувач натискає на кнопку вона повинна змінювати колір, 
-// якщо при цьому вже була обрана інша, вона повинна повернутися в початковий стан. 
+// Написати програму яка буде робити підсвічування активного елементу навігації
+// при кліці. Коли користувач натискає на кнопку вона повинна змінювати колір,
+// якщо при цьому вже була обрана інша, вона повинна повернутися в початковий стан.
 // Тобто активна (підсвічена) кнопка повинна бути лише одна.
 // index.html:
-
 
 // const list = document.querySelector('.js-nav');
 // list.addEventListener('click', (e)=>{
@@ -453,11 +422,8 @@
 // })
 
 // Створіть об'єкт JSON з інформацією про книгу. Книга повинна мати наступні поля:
-// назва книги автор книги рік видання жанр 
+// назва книги автор книги рік видання жанр
 // Перевірте, що ваш JSON є коректним за допомогою методу JSON.parse().
-
-
-
 
 // const book = `[
 //   {
@@ -471,7 +437,7 @@
 //     "author": "О.Уальд",
 //     "year": 1890,
 //     "genre": "Роман"
-//   }  
+//   }
 // ]`;
 
 // const object = {
@@ -493,8 +459,6 @@
 // console.log(update)
 // console.log(string)
 
-
-
 // const dishes = `[
 // {
 // "name": "pasta",
@@ -511,16 +475,12 @@
 // const parse = JSON.parse(dishes);
 // console.log(parse);
 
-
-
 // const btn = document.querySelector('.save');
 // const val = document.querySelector('.val');
-
 
 // btn.addEventListener('click', () => {
 //   localStorage.setItem("text", val.value);
 // })
-
 
 // const form = document.getElementById('form');
 
@@ -535,12 +495,10 @@
 //   form.reset()
 // })
 
-
-
-// Задача: Потрібно забезпечити плавне переміщення 
-// об'єкту при русі мишкою. Рішення: Використовуйте 
-// метод debounce з бібліотеки lodash. Встановіть час 
-// затримки в мілісекундах, наприклад 100мс, і передайте 
+// Задача: Потрібно забезпечити плавне переміщення
+// об'єкту при русі мишкою. Рішення: Використовуйте
+// метод debounce з бібліотеки lodash. Встановіть час
+// затримки в мілісекундах, наприклад 100мс, і передайте
 // функцію, яка буде виконуватися при переміщенні мишкою
 
 // import debounce from 'https://cdn.jsdelivr.net/npm/lodash.debounce@4.0.8/+esm';
@@ -553,7 +511,6 @@
 // }, 10);
 
 // document.addEventListener('mousemove', moveBox);
-
 
 // const input = document.getElementById('input');
 // const addBtn = document.getElementById('addBtn');
@@ -610,7 +567,6 @@
 
 // render();
 
-
 // start.addEventListener('click', () => {
 //   const delay = Number(time.value) * 1000;
 
@@ -619,9 +575,7 @@
 //   }, delay);
 // });
 
-
-
-// Створити таймер, який буде починати відлік з 10 секунд та зменшувати час кожну мілісекунду. 
+// Створити таймер, який буде починати відлік з 10 секунд та зменшувати час кожну мілісекунду.
 // При досягненні 5 секунд, таймер повинен змінювати колір фону сторінки на червоний.
 
 // const timerBox = document.querySelector('.timer');
@@ -638,11 +592,9 @@
 //   if(timerStart === 0){
 //   clearInterval(timer);
 //   timerBox.textContent = 'Час вийшов';
-  
+
 // }
 // }, 1);
-
-
 
 // Реалізуйте годинник, який показує поточний час у форматі "години:хвилини:секунди".
 //  Оновлюйте годинник кожну секунду без використання вбудованих функцій для отримання поточного часу.
@@ -664,9 +616,7 @@
 
 // setInterval(updateTime, 1000);
 
-
-
-// Напишіть функцію, яка обчислює скільки днів залишилося до 
+// Напишіть функцію, яка обчислює скільки днів залишилося до
 // наступного Дня народження на основі заданої дати народження.
 
 // function birthday(day, month){
@@ -685,12 +635,10 @@
 
 // console.log(birthday(5, 6))
 
-
 // const input = document.querySelector('.value');
 // const save = document.querySelector('.save');
 
 // const list = document.querySelector('.list');
-
 
 // let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
@@ -734,7 +682,6 @@
 
 // save.addEventListener('click', addItem);
 
-
 // const start = document.querySelector('#start');
 // const time = document.querySelector('#time');
 // const text = document.querySelector('#text');
@@ -746,8 +693,6 @@
 //     text.textContent = `Після нажаття кнопки пройшло ${time.value} секунд`;
 //   }, delay);
 // });
-
-
 
 // Налаштування профілю користувача
 // Створити форму, де користувач може налаштувати свій “віртуальний профіль”, а всі зміни одразу відображаються на сторінці.
@@ -781,7 +726,6 @@
 // - зібрати всі вибрані чекбокси в список
 // - визначити обраний radio
 
-
 // const nameInp = document.getElementById('name');
 // const ageInp = document.getElementById('age');
 // const color = document.getElementById('color');
@@ -798,7 +742,7 @@
 
 // color.addEventListener('input', () => {
 //   const colorVal = color.value;
-//   profileOutput.style.backgroundColor = colorVal;  
+//   profileOutput.style.backgroundColor = colorVal;
 // })
 
 // save.addEventListener('click', () => {
@@ -835,7 +779,7 @@
 //   <p>Стать: ${genderValue}</p>
 //   <p>Хоббі:${activesArr.join(', ')}</p>
 //   <p>Мова: ${language}</p>
-//   <p>Рівень мови: ${range.value}</p> 
+//   <p>Рівень мови: ${range.value}</p>
 //   `;
 
 //   localStorage.setItem('profile', JSON.stringify({
@@ -871,19 +815,10 @@
 //   `;
 // });
 
-
-
-
-
-
 // 3. Додаткові ускладнення:
 // Зберігати дані в localStorage, щоб після перезавантаження вони не зникали
 // Якщо користувач нічого не ввів — показати повідомлення про помилку
 // Додати кнопку “Скинути”
-
-
-
-
 
 // Завдання JavaScript:
 // При натисканні кнопки отримати:
@@ -893,45 +828,197 @@
 // - чи відмічений чекбокс
 // Якщо чекбокс не обраний - показати помилку
 // Якщо все ок - вивести результат у блок:
-// Дата: 2026-05-01  
-// Фото: avatar.png  
-// Файл: resume.pdf  
-// Статус: Підтверджено 
+// Дата: 2026-05-01
+// Фото: avatar.png
+// Файл: resume.pdf
+// Статус: Підтверджено
+
+// const dateInp = document.querySelector('.date');
+// const imgInp = document.querySelector('.img');
+// const fileInp = document.querySelector('.file');
+// const checkInp = document.querySelector('.check');
+// const saveBtn = document.querySelector('.save');
+// const box = document.querySelector('.block')
+
+// let checkState = false;
+// let fileName = null;
+// let imgName = null;
+
+// fileInp.addEventListener('change', () => {
+//   fileName = fileInp.files[0];
+// });
+
+// imgInp.addEventListener('change', () => {
+//   imgName = imgInp.files[0];
+// });
+
+// checkInp.addEventListener('change', () => {
+//   if(checkInp.checked){
+//     checkState = true;
+//   } else {
+//     alert('Погодьтесь з правилами');
+//   }
+// });
+
+// saveBtn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   box.innerHTML = `
+//   <p>Дата: ${dateInp.value}</p>
+//   <p>Назва фото: ${imgName.name}</p>
+//   <p>Назва файлу: ${fileName.name}</p>
+//   `
+// })
+
+// function numbers (arr){
+//   return new Promise((resolve, reject) => {
+//     const hasOdd = arr.some(num => num%2 !== 0);
+//     if(hasOdd){
+//       reject('Є непарні числа')
+//     } else {
+//       resolve('Непарних чисел немає')
+//     }
+//   })
+// }
+
+// numbers([2, 4, 6, 2, 8])
+// .then(console.log)
+// .catch(console.error);
+
+// numbers([3, 4, 6, 2, 8])
+// .then(console.log)
+// .catch(console.error);
+
+// Напишіть функцію, яка отримує масив об'єктів
+// і повертає проміс, який виконується з масивом імен, відсортованих за алфавітом.
+
+// const people = [
+//     { name: "John", age: 25 },
+//     { name: "Anna", age: 30 },
+//     { name: "Peter", age: 40 },
+//     { name: "Mary", age: 35 },
+//   ];
+
+//   function sort(people){
+//     return new Promise((resolve, reject) => {
+//       const peopleName = people.map(person => person.name)
+//       .sort((a, b) => a.localeCompare(b));
+//       resolve(peopleName);
+
+//       if(!people){
+//         reject('Данних немає');
+//       }
+//     })
+//   }
+
+//   sort(people)
+//   .then(console.log)
+//   .catch(console.error)
+
+// // Напишіть функцію rejectWithError, яка приймає
+// // повідомлення про помилку і повертає Promise, який відхиляється з вказаною помилкою.
+// function rejectWithError (errorMessage){
+//   return Promise.reject(errorMessage)
+// }
+
+// const errorMessage = 'Something went wrong';
+// rejectWithError(errorMessage)
+//   .catch((error) => {
+//     console.error(error);
+//    }) // Помилка "Something went wrong"
+
+// // Використовуючи Promise.all, створіть Promise, який об'єднує результати з трьох інших Promise.
+
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 3;
+// const promise3 = new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve('результат')
+//   }, 3000);
+// });
+
+// Promise.all([promise1, promise2, promise3])
+// .then(console.log)
+// .catch(console.error)
+
+// const promise1 = new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve('результат1')
+//   }, 3001);
+// });
+// const promise2 = new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve('результат2')
+//   }, 3000);
+// });
+
+// Promise.race([promise1, promise2])
+// .then(console.log)
+// .catch(console.error)
+
+// // Напишіть функцію raceTimeout,
+// // яка приймає Promise і таймаут (у мілісекундах)
+// // і повертає Promise, який вирішується, якщо початковий
+// // Promise вирішується до закінчення таймауту, або відхиляється,
+// // якщо таймаут закінчується перед вирішенням початкового Promise.
+// const fetchDataPromise = fetch("https://dog.ceo/api/breeds/image/random");
+// const timeout = 5000;
+
+// function raceTimeout(promise, timeout) {
+//   const timeoutPromise = new Promise((_, reject) => {
+//     setTimeout(() => {
+//       reject(new Error("час вийшов"));
+//     }, timeout);
+//   });
+//   return Promise.race([promise, timeoutPromise]);
+// }
+
+// raceTimeout(fetchDataPromise, timeout)
+//   .then((data) => {
+//     console.log(data); // Результат початкового Promise,
+//     // якщо він вирішується до закінчення таймауту
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   }); // Помилка "Timeout", якщо таймаут
+// // закінчився перед вирішенням початкового Promise
 
 
-const dateInp = document.querySelector('.date');
-const imgInp = document.querySelector('.img');
-const fileInp = document.querySelector('.file');
-const checkInp = document.querySelector('.check');
-const saveBtn = document.querySelector('.save');
-const box = document.querySelector('.block')
+// Напишіть функцію firstSuccessful, яка приймає масив промісів і повертає новий проміс:
+// - виконується (resolve) з результатом першого успішного промісу
+// - якщо всі проміси відхилені (reject) — повертає помилку "Всі проміси завершились з помилкою"
 
-let checkState = false;
-let fileName = null;
-let imgName = null;
+function firstSuccessful(promises) {
+    return Promise.any(promises)
+        .catch(() => {
+            throw new Error('Всі проміси завершились з помилкою');
+        });
+}
 
-fileInp.addEventListener('change', () => {
-  fileName = fileInp.files[0];   
+const promise1 = Promise.reject('error 1');
+const promise2 = Promise.resolve('Hello world');
+const promise3 = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('результат');
+    }, 3000);
 });
 
-imgInp.addEventListener('change', () => {
-  imgName = imgInp.files[0];
-});
+firstSuccessful([promise1, promise2, promise3])
+    .then(result => {
+        console.log('Результат:', result);
+    })
+    .catch(error => {
+        console.error('Помилка:', error.message);
+    });
 
-checkInp.addEventListener('change', () => {
-  if(checkInp.checked){
-    checkState = true;
-  } else {
-    alert('Погодьтесь з правилами');
-  }
-});
 
-saveBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  box.innerHTML = `
-  <p>Дата: ${dateInp.value}</p>
-  <p>Назва фото: ${imgName.name}</p>
-  <p>Назва файлу: ${fileName.name}</p>
-  `
-})
 
+const example1 = Promise.reject('err1');
+const example2 = Promise.reject('err2');
+
+firstSuccessful([example1, example2])
+    .then(result => {
+        console.log('Результат:', result);
+    })
+    .catch(error => {
+        console.error('Помилка:', error.message);
+    });
